6fdb2e59d3a3fdfe672fa045d76bfdf093b75940
Dhruvi Panchal <dhruvipanchal007@gmail.com>
2024-12-16 11:26:35 +0530

    trial

--- script.js
+++ script.js
@@ -14,6 +14,42 @@
 //     closeBtn.style.display = "none";
 // });
 
+// function setupToggleMenu() {
+//     const openBtn = document.querySelector("#open_btn");
+//     const closeBtn = document.querySelector("#close_btn");
+//     const navItems = document.querySelector("#nav-item");
+
+//     openBtn.addEventListener('click', () => {
+//         if (window.matchMedia("(max-width: 1024px)").matches) {
+//             navItems.style.display = 'block';
+//             openBtn.style.display = 'none';
+//             closeBtn.style.display = 'inline-block';
+//         }
+//     });
+
+//     closeBtn.addEventListener('click', () => {
+//         if (window.matchMedia("(max-width: 1024px)").matches) {
+//             navItems.style.display = 'none';
+//             openBtn.style.display = 'inline-block';
+//             closeBtn.style.display = 'none';
+//         }
+//     });
+
+//     window.addEventListener('resize', () => {
+//         if (!window.matchMedia("(max-width: 1024px)").matches) {
+//             navItems.style.display = 'block';
+//             openBtn.style.display = 'none';
+//             closeBtn.style.display = 'none';
+//         } else {
+//             navItems.style.display = 'none';
+//             openBtn.style.display = 'inline-block';
+//             closeBtn.style.display = 'none';
+//         }
+//     });
+// }
+
+// setupToggleMenu();
+
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
