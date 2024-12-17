81c0bd96b35e381ddf0cb850dce26173a453a81a
Dhruvi Panchal <dhruvipanchal007@gmail.com>
2024-12-17 11:49:57 +0530

    final done

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const navItems = document.querySelector("#nav-item");
-const openBtn = document.querySelector("#open_btn");
-const closeBtn = document.querySelector("#close_btn");
-
-openBtn.addEventListener('click', () => {
-    navItems.style.display = "flex";
-    openBtn.style.display = "none";
-    closeBtn.style.display = "inline-block";
-});
-
-const closeNav = () => {
-    navItems.style.display = "none";
-    openBtn.style.display = "inline-block";
-    closeBtn.style.display = "none";
-}
-
-closeBtn.addEventListener('click', (closeNav));
-
-
-
-
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
